3f00c81a11bb1ee70275438380a3712d3ed143ee
Alex Arobelidze <sandroarobeli77@gmail.com>
2020-10-29 01:57:12 -0500

    to improve modularity due to inheritance feature, switched to pug

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const hbs = require('hbs')
 require('./db/mongoose')
 
 
@@ -13,9 +12,12 @@ const catalogRouter = require('./routes/catalog')
 
 const app = express();
 
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -6,27 +6,33 @@ const Genre = require('../models/genre')
 
 // Display the site welcome page
 exports.index = async (req, res) => {
-     try {
-         const book_count = Book.countDocuments({})
-         const book_instance_count = BookInstance.countDocuments({}) 
-         const book_instance_available_count = BookInstance.countDocuments({ status: 'Available'})
-         const author_count = Author.countDocuments({})
-         const genre_count = Genre.countDocuments({})
-         const results = await Promise.all([book_count, book_instance_count, book_instance_available_count, author_count, genre_count ]) 
-         res.send({ 
-           book_count: results[0],
-           book_instance_count: results[1],
-           book_instance_available_count: results[2],
-           author_count: results[3],
-           genre_count: results[4]
-    }) 
-     } catch (error) {
-        res.send({ error: 'Unable to perform' })
-     }
+    try {
+        const book_count = Book.countDocuments({})
+        const book_instance_count = BookInstance.countDocuments({})
+        const book_instance_available_count = BookInstance.countDocuments({status: 'Available'})
+        const author_count = Author.countDocuments({})
+        const genre_count = Genre.countDocuments({})
+        const results = await Promise.all([book_count, book_instance_count, book_instance_available_count, author_count, genre_count])
+        res.render('index', {
+            title: 'Local Library Home',
+            book_count: results[0],
+            book_instance_count: results[1],
+            book_instance_available_count: results[2],
+            author_count: results[3],
+            genre_count: results[4]
+        })
+    } catch (error) {
+        res.render('index', { error } )
+    }
 };
 // Display list of all books.
-exports.book_list = (req, res) => {
-    res.send('Book list');
+exports.book_list = async (req, res, next) => {
+    try {
+      const books = await Book.find({}, 'title author').populate('author').exec()
+      res.render('book_list', {title: 'Book List', books})
+    } catch (error) {
+      return next(error) 
+    }
 };
 // Display detail page for a specific book.
 exports.book_detail = (req, res) => {
